d7f8e529e3718dcf9850c90318a01ee3b3a27972
sch3lp <sch3lp@gmail.com>
2023-02-25 10:07:14 +0100

    Added java17 toolchain + bumped junit and assertj libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
