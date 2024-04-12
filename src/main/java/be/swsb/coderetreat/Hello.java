3f4a5338a2c40c83a752461a8903d1ad482ffd7a
Youri Nijs <youri.nijs@yonicon.be>
2024-04-12 20:42:32 +0200

    bump gradle

--- src/main/java/be/swsb/coderetreat/Hello.java
+++ src/main/java/be/swsb/coderetreat/Hello.java
@@ -2,6 +2,10 @@ package be.swsb.coderetreat;
 
 public class Hello {
 
+    public static void main(String[] args) {
+        System.out.printf(new Hello().greet());
+    }
+
     public String greet() {
         return "World!";
     }
